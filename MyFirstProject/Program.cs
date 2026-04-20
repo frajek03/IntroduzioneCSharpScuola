618f56549f5d063f4dccbc61c94bd51d713189d2
frajek03 <francymd03@gmail.com>
2026-04-20 17:52:49 +0200

    First Commit

---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,56 @@
+﻿class Program
+{
+    
+    static int age = 20;
+
+    static int Age = 30;
+    public static void Main(string[] args)
+    {
+        //LessonOne();
+        VariablesOperationExamples();
+        //Console.WriteLine("Hello, World!");
+
+        //Console.WriteLine("age: " + age);
+        //Console.WriteLine("Age: " + Age);
+
+    }
+    public static void LessonOne()
+    {
+        //Console.Write("Ciao, inserisci il tuo nome: ");
+        //string? name = Console.ReadLine();
+        //Console.WriteLine($"Il tuo nome è {name}? che brutto nome.");
+
+        //Console.Write("inserisci n1: ");
+        //string? n1 =Console.ReadLine();
+
+        //Console.Write("inserisci n2: ");
+        //string? n2 = Console.ReadLine();
+
+        //int number1 = int.Parse(n1);
+        //int number2 = int.Parse(n2);
+
+        //Console.WriteLine($"Somma number1 + number2= {number1 + number2}");
+    }
+
+    public static void VariablesOperationExamples()
+
+    {
+        //Console.WriteLine($"Inserisci due numeri: ");
+        //int a = int.Parse(Console.ReadLine());
+        //int b = int.Parse(Console.ReadLine());
+
+        //Console.WriteLine($"\n-----Operazioni Aritmetiche\n");
+        //int sum = a + b;
+        //Console.WriteLine($"Somma di a e b = {sum}");
+        //int diff = a - b;
+        //Console.WriteLine($"Sottrazione di a e b = {diff}");
+        //double div = a / b;
+        //Console.WriteLine($"Divisione di a e b = {div}");
+        //int prod = a * b;
+        //Console.WriteLine($"Prodotto di a e b = {prod}");
+        //int resto = a % b;
+        //Console.WriteLine($"resto di a e b = {resto}");
+    }
+
+    
+}
